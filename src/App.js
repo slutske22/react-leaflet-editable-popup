06029b6182539ab46a52c06cfc583f2b60ea895b
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2020-01-16 15:31:55 -0800

    Using createRef(), createContext(), and setState to pass map context to sibling

--- src/App.js
+++ src/App.js
@@ -3,84 +3,40 @@
 
 import React from "react";
 import "./styles.css";
-import './components/ActiveArea.css'
 
-import { Map, TileLayer, Marker } from 'react-leaflet'
+import Map from './Map'
+import ExternalComponents from './externalComponents/ExternalComponents'
 
-import GeoSearch from './components/GeoSearch'
-import MousePosition from './components/MousePosition'
-import EditablePopup from './components/EditablePopup'
-import Polyline from './components/ArrowheadsPolyline'
-import RandomMarkersButtonLNative from './components/RandomMarkersButtonLNative'
-import { blackIcon } from './components/Icons'
-import ActiveArea from './components/ActiveArea'
-
-
-import { ensenadaBikePath } from './constants'
-
-const centerLat = 31.8117
-const centerLng = -116.6494
-
-
-// const mapReference = React.createRef()
-
-
+export const MapContext = React.createContext()
 
 class App extends React.Component{
 
-  componentDidMount(){
-    console.log(this.refs)
-  }
+  mapReference = React.createRef()
+  MapContext = React.createContext(this.mapReference)
 
+  // In order to properly pass the mapReference to the ExternalComponents, the following steps have to happen:
+  // 1. render runs()
+  // 2.  from render(), the mapReference is established
+  // 3.  componentDidMount runs
+  // 4. state is set in componentDidMount that the map is loaded and the mapReference is defined
+  // 5. ExternalComponents conditionally renders once state.mapLoaded is true, and receives the proper mapContext
+  // I had tried this approach with redux in my firestarter project, but it was too slow and there was a delay in sending the mapReference through the redux store and back out to the components.  This reference and context method is pretty fast.
+
+  state = { mapLoaded: false }
+
+  componentDidMount(){
+    this.setState({ mapLoaded:true })
+  }
 
   render(){
-
-
     return (
-
-
-      <Map id="mapId" center={[centerLat, centerLng]} zoom={11.5} maxZoom={14} zoomSnap='0.5' doubleClickZoom={false} ref={'mapReference'} >
-
-        <TileLayer
-          attribution={'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
-          url={"https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}"}
-          subdomains='abcd' ext='png'
-        />
-
-        <GeoSearch 
-          expanded={true} collapseAfterResult={false} useMapBounds={false} position={"topright"} 
-          providers={['arcgisOnlineProvider', 'geocodeServiceProvider']} />
-
-        <MousePosition position={'bottomleft'} />
-
-        <Marker position={[31.80, -116.79]}>
-          <EditablePopup editable removable open maxWidth="450">
-            <div>
-              <h3>Welcome to the react-leaflet custom components library</h3>
-              <section>This is an example page of some custom components.  You'll notice a Search bar, which is a custom component built from the <a href="https://esri.github.io/esri-leaflet/api-reference/controls/geosearch.html#result-object" target="_blank">Esri Leaflet Geocoder</a>.  You'll also see a mouseposition coordinates custom component in the corner.  Even this popup that you're reading is a custom component which has additional features beyond a normal popup.</section>
-            </div>
-          </EditablePopup>
-        </Marker>
-
-
-        <Polyline positions={ensenadaBikePath} arrowheads={ {size: '300m', frequency: '1000m'} } >
-          <EditablePopup removable editable nametag={'bike path'}>
-            <div>
-              <h3>Leaflet Arrowheads</h3>
-              <section>This polyline uses a customized version of &lt;Polyline /&gt; which enables the use of <a href="https://github.com/slutske22/leaflet-arrowheads" target="_blank">leaflet-arrowheads</a>.</section>
-            </div>
-          </EditablePopup>
-        </Polyline>
-
-        <ActiveArea />
-
-        <RandomMarkersButtonLNative position={'bottomright'} icon={blackIcon} />
-
-      </Map>
-
-
-    );
+      <MapContext.Provider value={this.mapReference}>
+        <Map mapReference={this.mapReference} ref={this.props.mapReference} />
+        {this.state.mapLoaded && <ExternalComponents mapReference={this.mapReference} />}
+      </MapContext.Provider>
+    )
   }
+
 }
 
 export default App
---
+++ src/externalComponents/ExternalComponents.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { MapContext } from '../App'
+
+class ExternalComponents extends React.Component{
+
+   componentDidMount(){
+      console.log(this.props.mapReference.current.contextValue)
+   }
+
+   render(){
+      return(
+         <MapContext.Consumer>
+            { (value) => {
+               console.log(value)
+            }}
+         </MapContext.Consumer>
+      )
+   }
+}
+
+export default ExternalComponents

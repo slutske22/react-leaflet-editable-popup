6e3e9d761cbf078639f08e3c0d404b31605b79d6
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2020-01-17 14:31:06 -0800

    filled out zoom control
    thanks to kboul on stackoverflow, plus added a disabled feature

--- src/externalComponents/ExternalComponents.jsx
+++ src/externalComponents/ExternalComponents.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MapContext from '../Context'
 import ActiveArea from './ActiveArea'
+import ZoomControl from './ZoomControl'
 
 import './ExternalComponents.css'
 
@@ -15,12 +16,12 @@ class ExternalComponents extends React.Component{
                <h4>External components</h4>
                <p>All components in this panel are not children of the <code>&lt;Map /&gt;</code> component.</p>
 
-               
+               <ZoomControl />
 
             </div>
 
             <ActiveArea>
-               <code>
+               <code style={{backgroundColor: 'rgba(255,255,255,0.7)'}}>
                   This activeArea is defined outside of the Map component, but communicates with it through the MapContext object.
                </code>
             </ActiveArea>

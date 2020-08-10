2745dbf57cb26bde0662100a486cb654fd025c89
Seth Lutske <seth.lutske@gmail.com>
2020-08-10 21:04:25 +0000

    setting up sidetab narrative

--- example/src/components/Sidebar.js
+++ example/src/components/Sidebar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { FiHome, FiChevronLeft, FiCompass, FiSettings } from "react-icons/fi";
+import { FiHome, FiChevronLeft, FiCompass, FiSettings, FiLayers } from "react-icons/fi";
+import { BsReverseLayoutTextSidebarReverse, BsController } from 'react-icons/bs'
+import { GoLinkExternal } from 'react-icons/go'
 import { Sidebar, Tab } from '../../../components/external/Sidebar'
 
 const SidebarComponent = ({ map }) => {
@@ -29,6 +31,18 @@ const SidebarComponent = ({ map }) => {
 
             <Tab id="home" header="Home" icon={<FiHome />} active>
                <p>
+                  Welcome to the React-Leaflet V3 custom components library.  This example page shows off some of the custom components that have been built and adapted for react-leaflet v3.  
+               </p>
+               <p>
+                  Click the icons on the left to learn more about the various types of custom components offered here.
+               </p>
+               <p>
+                  Check out the github page for more information.
+               </p>
+            </Tab>
+
+            <Tab id="sidebar" header="Sidebar" icon={<BsReverseLayoutTextSidebarReverse />}>
+               <p>
                   This sidebar is adapted from <a href="https://github.com/eferhatg/react-leaflet-sidetabs" target="_blank" rel="noopener noreferrer">react-leaflet-sidetabs</a> to be compatible with react-leaflet version 3.
                </p>
                <p>
@@ -57,51 +71,33 @@ const SidebarComponent = ({ map }) => {
                   For more react-leaflet-v3 custom components, visit my <a href="https://github.com/slutske22/react-leaflet-custom-components" target="_blank" rel="noopener noreferrer">custom components library</a>.
                </p>
             </Tab>
-            <Tab id="props" header="Props" icon={<FiCompass />}>
 
-               <h3>id</h3>
-               <h4>string</h4>
-               <p>CSS id of the sidebar element. Defaults to <code>'leaflet-sidebar'</code></p>
-               
-               <h3>position <span className="required">required</span></h3>
-               <h4>string: "left" or "right"</h4>
-               <p>determines the placement of the sidebar on the screen</p>
-
-               <h3>closeIcon <span className="required">required</span></h3>
-               <h4>string | react element</h4>
-               <p>the icon element show in the upper corner to close the sidetabs</p>
-
-               <h3>map</h3>
-               <h4>leaflet map instance</h4>
-               <p>the leaflet map instance you want to connect to the sidetabs</p>
-
-               <h3>collapsed <span className="required">required</span></h3>
-               <h4>boolean</h4>
-               <p>the open/close state of the sidetabs</p>
-
-               <h3>selected <span className="required">required</span></h3>
-               <h4>string</h4>
-               <p>the id of the currently open tab</p>
-
-               <h3>onOpen <span className="required">required</span></h3>
-               <h4>function</h4>
-               <p>callback function when sidetabs are toggled open</p>
-
-               <h3>onClose <span className="required">required</span></h3>
-               <h4>function</h4>
-               <p>callback function when sidetabs are toggled closed</p>
-
-               <h3>panMapOnChange</h3>
-               <h4>boolean</h4>
-               <p>Whether or not to adjust the map center with the opening and closing of the sidetabs. Requires the <code>map</code> prop to work.</p>
-
-               <h3>rehomeControls</h3>
-               <h4>boolean</h4>
-               <p>Whether or not to automatically adjust control elements to align with the sidetabs</p>
-
+            <Tab id="controls" header="External Components" icon={<GoLinkExternal />}>
+               <p>
+                  Many times it is useful to create components outside of the <code>&lt;MapContainer&gt;</code> component.
+               </p>
             </Tab>
+
+
+            <Tab id="layers" header="Custom Layer Types" icon={<FiLayers />}>
+               <p>Some content in here</p>
+            </Tab>
+
+            <Tab id="settings" header="Settings" icon={<FiSettings />}>
+               <p>Some content in here</p>
+            </Tab>
+
+            <Tab id="settings" header="Settings" icon={<FiSettings />}>
+               <p>Some content in here</p>
+            </Tab>
+
+            <Tab id="settings" header="Settings" icon={<FiSettings />}>
+               <p>Some content in here</p>
+            </Tab>
+
+
             <Tab id="settings" header="Settings" icon={<FiSettings />} anchor="bottom">
-               <p>The button for this tab can be anchored to the bottom by using the <code>anchor="bottom"</code> props on the <code>Tab</code> component</p>
+               <p>Some content in here</p>
             </Tab>
 
          </Sidebar>
